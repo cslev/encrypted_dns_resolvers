49680e002dece72b1074f149735e3f6c4a024ee3
cslev <cs.lev@gmx.com>
2023-05-03 16:33:31 +0800

    add helper scripts

---
+++ scripts/generate_json.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+ROOT="$(dirname "$0")"
+source $ROOT/extra.sh
+
+c_print "White" "Get the DoH resolvers from CURL github wiki using scrape-doh-providers.py script..."
+d=$(date '+%Y%m%d')
+python3 scrape-doh-providers.py 'o["name"],o["url"]' >doh_resolvers_data_curl_${d}.lst
+
+c_print "White" "Convert downloaded data into simple csv..."
+cat doh_resolvers_data_curl_${d}.lst |sed -e "s/(//g" |sed -e "s/)//g" |sed -e "s/'//g" |sed -e "s/ //g" > doh_resolvers_data_curl_${d}.csv
+
+
+num_resolvers=$(cat doh_resolvers_data_curl_${d}.csv|wc -l)
+output=doh_resolvers_data_${d}.json
+
+c_print "White" "Creating JSON... ${output}"
+echo "{" > $output
+
+id=1
+#percent=$(echo "scale=2;${id}/${num_resolvers}|bc -l")
+
+for i in $(cat doh_resolvers_data_curl_${d}.csv)
+do
+  percent=$(echo "scale=2; ${id}/${num_resolvers}*100"|bc -l)
+  echo -ne "[ processing... ] ${percent}%" $"\r" 
+  
+  #first row of an entry
+  name=$(echo $i|cut -d ',' -f 1)
+  echo -e "\t\"${name}\" : {" >> $output 
+  
+  # row ID
+  echo -e "\t\t\"id\": ${id}," >> $output
+  uri=$(echo $i|cut -d ',' -f 2)
+
+  #row name
+  echo -e "\t\t\"name\": \"${name}\"," >> $output
+
+  #row uri
+  echo -e "\t\t\"uri\": \"${uri}\"," >> $output
+
+  #row bootstrap
+  base_domain=$(echo $uri|cut -d '/' -f 3)
+  # this can return more than one IP, so we make multiple bootstrap rows accordingly (if needed)
+  bootstrap_id=1
+  bootstrap_id_max=$(dig +short $base_domain A |grep -v '\.$' |wc -l)
+  #in case the DNS resolution fails,i.e., no IP returned we add bootstrap_id: UNKNOWN
+  #check the bootstrap_id_max, if it is 0, then no IP returned
+  if [ $bootstrap_id_max -eq 0 ]
+  then
+    echo -e "\t\t\"bootstrap_${bootstrap_id}\": \"UNKNOWN\"" >> $output
+  else
+    for ip in $(dig +short $base_domain A |grep -v '\.$') #the last grep gets rid of CNAME records
+    do
+      if [ $bootstrap_id -ne $bootstrap_id_max ]
+      then
+        #we need to print a ',' at the end if further bootstrap_ids are about to be added
+        echo -e "\t\t\"bootstrap_${bootstrap_id}\": \"${ip}\"," >> $output
+      else
+        #after the last bootstrap id, we don't print ',' at the end
+        echo -e "\t\t\"bootstrap_${bootstrap_id}\": \"${ip}\"" >> $output
+      fi
+      bootstrap_id=`expr $bootstrap_id + 1`
+    done
+  fi
+
+
+  if [ $id -ne $num_resolvers ]
+  then
+    #we need to print additional ',' after each entry
+    echo -e "\t}," >> $output
+  else
+    #after the last entry, we don't print ','!
+    echo -e "\t}" >> $output
+  fi
+
+  #increment id 
+  id=`expr $id + 1`
+done
+echo "}" >> $output
---
+++ scripts/scrape-doh-providers.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+#
+###################################################################################################
+# Written by kimbo, downloaded from: https://gist.github.com/kimbo/dd65d539970e3a28a10628f15398247b
+###################################################################################################
+#
+# Scrape Doh provider URLs from Curl's DNS-over-HTTPS wiki (https://raw.githubusercontent.com/wiki/curl/curl/DNS-over-HTTPS).
+# 
+# Example usage: ./scrape_doh_providers.py '"{} - {}".format(o["url"], o["name"])'
+#
+import argparse
+import re
+import urllib.request
+
+HTTPS_URL_RE = re.compile(r'https://'
+                          r'(?P<hostname>[0-9a-zA-Z._~-]+)'
+                          r'(?P<port>:[0-9]+)?'
+                          r'(?P<path>[0-9a-zA-Z._~/-]+)?')
+
+PROVIDER_RE = re.compile(r'(\[([^\]]+)\]\(([^)]+))\)|(.*)')
+
+# URLs that are not Doh URLs
+do_not_include = ['my.nextdns.io', 'blog.cloudflare.com', 'https://blog.cloudflare.com/welcome-hidden-resolver', 'https://my.nextdns.io/start']
+
+
+def get_doh_providers():
+    found_table = False
+    with urllib.request.urlopen('https://raw.githubusercontent.com/wiki/curl/curl/DNS-over-HTTPS.md') as fp:
+        for line in fp:
+            line = line.decode()
+            if line.startswith('|'):
+                if not found_table:
+                    found_table = True
+                    continue
+                cols = line.split('|')
+                provider_col = cols[1].strip()
+                website = None
+                provider_name = None
+                matches = PROVIDER_RE.findall(provider_col)
+                if matches[0][3] != '':
+                    provider_name = matches[0][3]
+                if matches[0][1] != '':
+                    provider_name = matches[0][1]
+                if matches[0][2] != '':
+                    website = matches[0][2]
+                if provider_name is not None:
+                    provider_name = re.sub(r'([^[]+)\s?(.*)', r'\1', provider_name)
+                    while provider_name[-1] == ' ':
+                        provider_name = provider_name[:-1]
+                if len(cols) < 3:
+                    continue
+                url_col = cols[2]
+                doh_url_matches = HTTPS_URL_RE.findall(url_col)
+                if len(doh_url_matches) == 0:
+                    continue
+                else:
+                    for doh_url in doh_url_matches:
+                        if doh_url[0] in do_not_include:
+                            continue
+                        yield {
+                            'name': provider_name,
+                            'website': website,
+                            'url': 'https://{}{}{}'.format(doh_url[0], ':{}'.format(doh_url[1]) if len(doh_url[1]) != 0 else '', doh_url[2]),
+                            'hostname': doh_url[0],
+                            'port': doh_url[1] if len(doh_url[1]) != 0 else '443',
+                            'path': doh_url[2],
+                        }
+            if found_table and line.startswith('#'):
+                break
+    return
+
+def main():
+    # example: ./scripts/scrape_doh_providers.py '"{} - {}".format(o["url"], o["name"])'
+    parser = argparse.ArgumentParser(description='A script to parse DoH provider URLs from cURL\'s wiki page!')
+    parser.add_argument('format', help='Format of output. Example: \'*(o["url"],o["name"])\'', default='o["url"]',
+                        nargs='?')
+    args = parser.parse_args()
+    for o in get_doh_providers():
+        print(eval(args.format))
+
+
+if __name__ == '__main__':
+    main()
